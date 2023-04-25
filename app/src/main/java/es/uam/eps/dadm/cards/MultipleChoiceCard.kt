91458f7737d665a9c1f1750265af3e20be1560da
Jakob Vetter <jakob.vetter@tum.de>
2023-04-25 16:26:58 +0200

    EDIT: Edit MultipleChoiceCard so it correctly extends Card

--- app/src/main/java/es/uam/eps/dadm/cards/MultipleChoiceCard.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/MultipleChoiceCard.kt
@@ -1,11 +1,24 @@
 package es.uam.eps.dadm.cards
 
+import java.time.LocalDateTime
+import java.util.*
+
 class MultipleChoiceCard(
     question: String,
-    var answers: MutableMap<String, Boolean>
-) : Card(question, "") {
+    answer: String,
+    date: String = LocalDateTime.now().toString(),
+    id: String = UUID.randomUUID().toString(),
+    deckId: Long = 0,
+    val answerOptions: Map<String, Boolean>
+) : Card(question, answer, date, id, deckId) {
+
+    init {
+        if (!answerOptions.containsValue(true)) {
+            throw IllegalArgumentException("At least one answer option must be marked as correct")
+        }
+    }
 
     fun checkAnswer(answer: String): Boolean {
-        return this.answers.getValue(answer)
+        return this.answerOptions.getValue(answer)
     }
 }
