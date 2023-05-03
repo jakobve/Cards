58fe4d17b8dc005715a9125cf429abc794bffd8f
Jakob Vetter <jakob.vetter@tum.de>
2023-05-03 13:52:51 +0200

    FIX: Code sanity DeckListViewModel

--- app/src/main/java/es/uam/eps/dadm/cards/DeckListViewModel.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/DeckListViewModel.kt
@@ -2,20 +2,19 @@ package es.uam.eps.dadm.cards
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import com.google.firebase.database.FirebaseDatabase
 import timber.log.Timber
 
 class DeckListViewModel(application: Application): AndroidViewModel(application) {
     private val context = getApplication<Application>().applicationContext
 
-    val decksWithCards: LiveData<List<DeckWithCards>> = CardsApplication.getDecksWithCardsForUser(context, getUserId()!!)
+    val decksWithCards = CardsApplication.getDecksWithCardsForUser(context, getUserId()!!)
 
     fun uploadDecks() {
         Timber.i("Upload decks")
-        var deckReference = FirebaseDatabase.getInstance().getReference("decks")
-        var deckList = mutableListOf<Deck>()
-        for(deckWithCards in decksWithCards!!.value!!) {
+        val deckReference = FirebaseDatabase.getInstance().getReference("decks")
+        val deckList = mutableListOf<Deck>()
+        for(deckWithCards in decksWithCards.value!!) {
             deckList.add(deckWithCards.deck)
         }
         deckReference.setValue(deckList)
@@ -23,9 +22,9 @@ class DeckListViewModel(application: Application): AndroidViewModel(application)
 
     fun uploadCards() {
         Timber.i("Upload cards")
-        var cardReference = FirebaseDatabase.getInstance().getReference("cards")
-        var cardList = mutableListOf<Card>()
-        for(deckWithCards in decksWithCards!!.value!!) {
+        val cardReference = FirebaseDatabase.getInstance().getReference("cards")
+        val cardList = mutableListOf<Card>()
+        for(deckWithCards in decksWithCards.value!!) {
             for(card in deckWithCards.cards) {
                 cardList.add(card)
             }
